7409e9d7e2fb293d57bb668d52e93fc40d8841c3
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-05-18 15:10:54 +0100

    ribbon

---
+++ Discovery-Service/src/test/java/de/tekup/eureka/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tekup.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ User-Service/src/main/java/de/tekup/user/controllers/UserController.java
@@ -0,0 +1,19 @@
+package de.tekup.user.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	@Autowired
+	private Environment env;
+	@GetMapping
+	public String works() {
+		return "User MS works on port :"+env.getProperty("local.server.port");
+	}
+
+}
